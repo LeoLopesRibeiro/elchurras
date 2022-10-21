1c8c3ce401620b49b2863d8e5fcd873d74df0877
Leonardo Lopes <leolopesribeiro05@gmail.com>
2022-10-20 22:54:01 -0300

    Geolocalização completa

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Locacao from './src/pages/Locacao';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+return(
+        <View>
+       <Locacao/>
     </View>
-  );
+)
 }
 
 const styles = StyleSheet.create({
